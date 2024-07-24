64e771ca4c8fdb4e0d4d8c379c1957c86c9b249c
TIDJANI BACHIR <144597750+Tidjani1Bachir@users.noreply.github.com>
2024-07-24 19:46:55 +0100

    Add files via upload

---
+++ app/index.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+const router = useRouter();
+
+const Home = () => {
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.menu}
+              dimension="60%"
+            ></ScreenHeaderBtn>
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn
+              iconUrl={images.profile}
+              dimension="100%"
+            ></ScreenHeaderBtn>
+          ),
+          headerTitle: "",
+        }}
+      ></Stack.Screen>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`/search/${searchTerm}`);
+              }
+            }}
+          ></Welcome>
+
+          <Popularjobs></Popularjobs>
+
+          <Nearbyjobs></Nearbyjobs>
+
+
+
+
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
